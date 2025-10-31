9f7210ccb981734ccf5b8431bf1540d59cc42a83
blessing_u <ubiomorblessing@gmail.com>
2025-11-01 00:29:05 +0100

    feat: add Footer component to layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer";
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={`${manrope.className} antialiased`}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
